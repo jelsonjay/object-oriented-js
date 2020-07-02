3854cea5c48b24bf807a8f52ef3f093c4eec011e
jelson <jelson20@outlook.com>
2020-07-02 23:48:49 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -23,12 +23,18 @@ function itManager(...parm) {
 // Prototype Inheritance
 itManager.prototype = Object.create(Staff.prototype);
 
+// Remove
+itManager.prototype.removeStaff = function (s) {
+	staffs = staffs.filter((staff) => {
+		return staff.email != s.email;
+	});
+};
+
+let marketing = new Staff('Sara Tyson', 'sara19@gmail.com', 'Marketing');
+let juniorDev = new Staff('James Ryan', 'james@gmail.com', 'Junior Developer');
 let tester = new Staff('Phil Jame', 'phil@outlook.com', 'Tester');
 let web_design = new Staff('Michael Ross', 'web24@outlook.com', 'Web Design');
-let it_manager = new Staff(
-	'Alberto Pedro',
-	'alberto@outlook.com',
-	'IT Manager'
-);
+let it_manager = new Staff('Alberto P', 'alberto@outlook.com', 'IT Manager');
+let staffs = [tester, web_design, marketing, juniorDev, it_manager];
 
 console.log(it_manager);
