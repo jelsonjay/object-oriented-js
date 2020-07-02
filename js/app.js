8f77b92fa8471f8a365a04b18afd29424a0cfed6
jelson <jelson20@outlook.com>
2020-07-02 23:00:14 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,13 +3,20 @@
 function Staff(name, email, job_title) {
 	(this.name = name), (this.email = email);
 	this.job_title = job_title;
-	this.signin = function () {
-		console.log(this.name, 'username as signin');
-	};
+	this.signin = function () {};
 }
 
+Staff.prototype.signin = function () {
+	this.online = true;
+	console.log(this.name, 'username as signin');
+};
+Staff.prototype.signout = function () {
+	this.online = true;
+	console.log(this.name, 'username as signout');
+};
+
 let tester = new Staff('Phil J', 'phil@outlook.com', 'Tester');
-let web_design = new Staff('Steson Web', 'web24@outlook.com', 'Web Design');
+let web_design = new Staff('Steveson Web', 'web24@outlook.com', 'Web Design');
 
 console.log(tester);
 web_design.signin();
